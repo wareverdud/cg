2b3c4b8fec511c40aaaef079029bca3a09e8b44f
wareverdud <71446364+wareverdud@users.noreply.github.com>
2023-10-16 01:24:10 +0300

    rasterization

--- src/utils/resource_utils.cpp
+++ src/utils/resource_utils.cpp
@@ -7,16 +7,15 @@
 
 #include <stb_image_write.h>
 
-
 std::string view_command(const std::filesystem::path& path)
 {
 #ifdef __APPLE__
 	return std::string("open ").append(path.string());
 #endif
-#ifdef __WINDOWS__
+#ifdef _WIN32
 	return std::string("start ").append(path.string());
 #endif
-	return path.string();
+	return "";
 }
 
 void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, const std::filesystem::path& filepath)
@@ -31,6 +30,8 @@ void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, c
 	if (result != 1)
 		THROW_ERROR("Can't save the resource");
 
-	std::system(view_command(filepath).c_str());
+	auto command = view_command(filepath);
+	if (!command.empty())
+		std::system(command.c_str());
 }
 
